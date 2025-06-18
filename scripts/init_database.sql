449fd7cce94d8c4e7587a4128ec1582ecc340728
Nathan Bvumbwe <151523096+NathanBvumbwe@users.noreply.github.com>
2025-06-18 20:24:43 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -23,4 +23,63 @@ CREATE SCHEMA profiles;
 CREATE SCHEMA sales;
 CREATE SCHEMA products;
 
+--CREATION OF TABLES IN THE SCHEMAS
+
+--tbale for products
+CREATE TABLE products.products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    category VARCHAR(100),
+    price DECIMAL(10, 2),
+    discountPercentage DECIMAL(5, 2),
+    rating DECIMAL(3, 2),
+    stock INT,
+    tags TEXT[], 
+    brand VARCHAR(100),
+    sku VARCHAR(100),
+    weight DECIMAL(10, 2),
+    dimensions VARCHAR(100), 
+    warrantyInformation TEXT,
+    shippingInformation TEXT,
+    availabilityStatus VARCHAR(50),
+    reviews JSONB, 
+    returnPolicy TEXT,
+    minimumOrderQuantity INT,
+    images TEXT[], 
+    thumbnail VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+--table for user profiles
+CREATE TABLE profiles.user_profiles (
+    customer_id SERIAL PRIMARY KEY,
+    age INT,
+    location VARCHAR(255),
+    salary DECIMAL(10, 2),
+    race VARCHAR(100),
+    gender VARCHAR(10),
+    education VARCHAR(100)
+);
+
+--tabl for sales
+CREATE TABLE sales.sales (
+    sale_id SERIAL PRIMARY KEY,
+    date DATE,
+    product VARCHAR(255),
+    quantity INT,
+    unit_price DECIMAL(10, 2),
+    total_price DECIMAL(10, 2),
+    region VARCHAR(100),
+    sales_rep VARCHAR(100),
+    customer_id INT REFERENCES profiles.user_profiles(customer_id)  -- Foreign key reference
+);
+
+
+--LOADING DATA INTO THE SPECIFIC TABLES OF OUR WAREHOUSE
+
+
+
+
+
 
