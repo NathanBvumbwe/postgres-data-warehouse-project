b1af02596b0f07a4b5a3d8e4310d4fa9ca578632
Nathan Bvumbwe <151523096+NathanBvumbwe@users.noreply.github.com>
2025-06-18 10:38:56 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,5 +1,7 @@
---so this is the data_warehouse am building for my data virtualization assignment. abit messy and ugly but not bad for a start 
-	
+/*--so this is the data_warehouse am building for my data virtualization assignment. 
+abit messy and ugly but not bad for a start, loooking forward for more projects to come.
+am using postegres so not much code in here. but trust me i know what am doing😂😂.
+	*/
 -- Database: data_warehouse
 
 -- DROP DATABASE IF EXISTS data_warehouse;
