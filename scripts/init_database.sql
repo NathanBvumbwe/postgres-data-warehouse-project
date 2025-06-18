36f52c196fb64c15f46329349d74297bdcd2e0fe
Nathan Bvumbwe <151523096+NathanBvumbwe@users.noreply.github.com>
2025-06-18 10:29:06 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+-- Database: data_warehouse
+
+-- DROP DATABASE IF EXISTS data_warehouse;
+
+CREATE DATABASE data_warehouse
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+	
+--CREATING SCHEMAS TO BETTER ORGANIZE THE DATA YOU KNOW
+CREATE SCHEMA profiles;
+CREATE SCHEMA sales;
+CREATE SCHEMA products;
+
 
